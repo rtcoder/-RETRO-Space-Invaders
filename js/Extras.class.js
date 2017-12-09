f46346b072ff78ea2288ef9e43fdb6c098f7c939
Dawid Jeż <dawidjez@gmail.com>
2017-12-09 03:21:56 +0100

    basic classes

---
+++ js/Extras.class.js
@@ -0,0 +1,58 @@
+class Extras {
+    constructor() {
+        this.types = [
+            {
+                name: 'doubleShoot',
+                remaningTime: 10000
+            },
+            {
+                name: 'largeShip',
+                remaningTime: 10000
+            },
+            {
+                name: 'superSpeed',
+                remaningTime: 10000
+            }
+        ];
+        this.activeExtras = [];
+        this.interval = 3000;
+        this.list = [];
+        this.lastDropTime = null;
+        this.step = 2;
+    }
+
+    move() {
+        let extras = this.list;
+        for (let i = 0; i < extras.length; i++) {
+            extras[i].y += this.step;
+            if (extras[i].y > canvas.height) {
+                extras.splice(i, 1);
+            }
+        }
+    }
+
+    addPackage() {
+        if (new Date().getTime() - this.lastDropTime > this.interval || !this.lastDropTime) {
+            let count = 0;
+            let c = 0;
+            let x = getRandomInt(1, canvas.width);
+            let y = getRandomInt(1, canvas.height / 3);
+            let p = new Package(x, y);
+            this.list.push(p);
+            this.lastDropTime = new Date().getTime();
+        }
+    }
+    countDown() {
+        let ae = this.activeExtras;
+        for (let i in ae) {
+            ae[i].remaningTime -= Game.loopMilisconds;
+            if (ae[i].remaningTime <= 0) {
+                ae.splice(i, 1);
+                document.getElementById(i + 'Container').style = 'display:none';
+            } else {
+                document.getElementById(i).innerHTML = Math.floor(ae[i].remaningTime / 1000) + 's';
+                document.getElementById(i + 'Container').style = 'display:inline';
+            }
+        }
+    }
+}
