c9a67b46ce220ab4877020a82b5df8683cfff72e
Dawid Jeż <dawidjez@gmail.com>
2018-09-20 23:41:31 +0200

    gamepad

---
+++ js/Gamepad.class.js
@@ -0,0 +1,23 @@
+class Gamepad {
+    constructor() {
+        this.right = false;
+        this.left = false;
+        this.wheelClick = false;
+        this.scrollDown = false;
+        this.scrollUp = false;
+        this.xPos = 0;
+        this.yPos = 0;
+
+        this.setEvents();
+    }
+
+    setEvents() {
+        (function () {
+            window.addEventListener("gamepadconnected", function (e) {
+                console.log("Gamepad connected at index %d: %s. %d buttons, %d axes.",
+                    e.gamepad.index, e.gamepad.id,
+                    e.gamepad.buttons.length, e.gamepad.axes.length);
+            });
+        })();
+    }
+}
